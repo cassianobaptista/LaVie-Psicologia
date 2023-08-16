4303504585bcd6dc6a8dce943c2cbef8a6ba93b5
Cassiano T. Baptista <cassianotbap@gmail.com>
2023-08-16 05:06:29 -0300

    feat: inclusão do handleError

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const routes = require("./routes/index");
+const handleError = require("./middlewares/handleError");
 const db = require("./database/index");
-
 const app = express();
 
 db.hasConnection();
@@ -9,5 +9,7 @@ db.hasConnection();
 app.use(express.json());
 
 app.use(routes);
+
+app.use(handleError);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
