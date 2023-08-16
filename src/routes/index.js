c962416b01781576d021eebd4750661657deed92
Cassiano T. Baptista <cassianotbap@gmail.com>
2023-08-16 05:12:24 -0300

    feat: autenticação para login de psicólogos e para registro de atendimentos

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const pacientesController = require("../controllers/pacientesController");
 const psicologosController = require("../controllers/psicologosController");
 const atendimentosController = require("../controllers/atendimentosController");
+const authController = require("../controllers/authController");
+
+const psicologoCreateValid = require("../validations/psicologos/create");
+const authLoginValid = require ("../validations/psicologos/login");
+const auth = require("../middlewares/auth");
 const routes = express.Router();
 
 // Pacientes
@@ -14,14 +19,15 @@ routes.put("/pacientes/:id", pacientesController.atualizarPaciente);
 // Psicologos
 routes.get("/psicologos", psicologosController.listarPsicologos);
 routes.get("/psicologos/:id", psicologosController.listarPsicologoPorId);
-routes.post("/psicologos", psicologosController.cadastrarPsicologo);
+routes.post("/psicologos", psicologoCreateValid, psicologosController.cadastrarPsicologo);
+routes.post("/psicologos/login", authLoginValid, authController.login, )
 routes.delete("/psicologos/:id", psicologosController.deletarPsicologo);
 routes.put("/psicologos/:id", psicologosController.atualizarPsicologo);
 
 // Atendimentos
 routes.get("/atendimentos", atendimentosController.listarAtendimentos);
 routes.get("/atendimentos/:id", atendimentosController.listarAtendimentoPorId);
-routes.post("/atendimentos", atendimentosController.cadastrarAtendimento);
+routes.post("/atendimentos", auth, atendimentosController.cadastrarAtendimento);
 
 
 module.exports = routes;
